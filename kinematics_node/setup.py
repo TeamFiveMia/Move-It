2c7f503a0e0493d30d82206c779eeee1730b04b4
joudyahmedd10 <joudyahmedd10@gmail.com>
2026-09-04 22:50:33 +0300

    ..

--- kinematics_node/setup.py
+++ kinematics_node/setup.py
@@ -23,7 +23,7 @@ setup(
         ],
     },
     entry_points={
-        'console_scripts': [
+        'console_scripts': [ 'kinematics_node = kinematics_node.kinematics_node:main',
         ],
     },
 )
