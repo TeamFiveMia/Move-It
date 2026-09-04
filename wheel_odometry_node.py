533ec88a6858768a3c8f5b24f956bf8d807756e8
rokaia1692006 <rokaia.moh.a@gmail.com>
2026-09-04 14:16:29 +0000

    math for position

--- wheel_odometry_node.py
+++ wheel_odometry_node.py
@@ -1,7 +1,9 @@
 
 import rclpy
-from rcplpy.node import Node
+from rclpy.node import Node
 from robot_kinematics import DiffDriveKinematics,MecanumKinematics,ThreeWheelOmniKinematics,FourWheelOmniKinematics
+from std_msgs.msg import Float64MultiArray
+from nav_msgs.msg import Odometry
 class WheelOdometryNode(Node):
     def __init__(self):
         super().__init__('wheel_odometry_node')
@@ -23,7 +25,7 @@ class WheelOdometryNode(Node):
         self.wheel_radius = self.get_parameter('wheel_radius').value
         # check whick kinematics is used
         if self.drive_type == 'DiffDriveKinematics':
-            self.kinematics = DiffDriveKinematics(self.track_width,self.wheel_radius)
+            self.kinematics = DiffDriveKinematics(self.track_width,self.wheelbase,self.wheel_radius)
         elif self.drive_type == 'MecanumKinematics':
             self.kinematics = MecanumKinematics(self.track_width,self.wheelbase,self.wheel_radius)
         elif self.drive_type == 'ThreeWheelOmniKinematics':
@@ -32,4 +34,43 @@ class WheelOdometryNode(Node):
             self.kinematics = FourWheelOmniKinematics(self.track_width,self.wheelbase,self.wheel_radius)
         else:
             self.get_logger().error('Wrong type')
+        #subscriber + publisher
+        self.subscribe = self.create_subscription(Float64MultiArray,'/encoder_speeds',self.encoder_callback,10)
+        self.pub = self.create_publisher(Odometry,'/odom',10)
+    def encoder_callback(self,msg):
+        #current time
+        curr = self.get_clock().now()
+        # difference
+        diff  = (curr - self.time).nanoseconds * 1e-9
+        self.time = curr
+        if diff <=0:
+            return
+        #get forward kinematics 
+        vx,vy,w=self.kinematics.forward(msg.data)
+        # integration
+        dx = (vx * cos(self.yaw) - vy * sin(self.yaw)) * diff
+        dy = (vx * sin(self.yaw) + vy * cos(self.yaw)) * diff
+        dyaw = w * diff
+        #put in odom vars
+        self.x += dx
+        self.y += dy
+        self.yaw += dyaw
+        #publish new values
+        msg = Odometry()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.header.frame_id = 'odom'
+        msg.pose.pose.position.x = self.x
+        msg.pose.pose.position.y = self.y
+        msg.pose.pose.orientation.z = sin(self.yaw/2.0)
+        msg.pose.pose.orientation.w = cos(self.yaw/2.0)
+        msg.child_frame_id = 'base_link'
+        msg.twist.twist.linear.x = vx
+        msg.twist.twist.linear.y = vy
+        msg.twist.twist.angular.z = w
+        self.pub.publish(msg)
         
+
+        
+
+        
+
